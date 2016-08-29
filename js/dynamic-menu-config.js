19adb6bba78bdfc1eaedc41617712ed91653e54c
tribunskiy <forty-7@yandex.ru>
2016-08-29 13:53:47 +0300

    update

---
+++ js/dynamic-menu-config.js
@@ -0,0 +1,33 @@
+/*
+ <ul>
+ <li class="active">
+ <a href="index.html" title="Dashboard"><i class="fa fa-lg fa-fw fa-home"></i> <span class="menu-item-parent">Home</span></a>
+ </li>
+ <li>
+ <a href="projects.html" title="Projects"><i class="fa fa-file-text-o"></i> <span class="menu-item-parent">Projects</span></a>
+ </li>
+ <!--<li>-->
+ <!--<a href="blog.html" title="Blog"><i class="fa fa-paragraph"></i> <span class="menu-item-parent">Blog</span></a>-->
+ <!--</li>-->
+ <!--<li>-->
+ <!--<a href="gallery.html" title="Gallery"><i class="fa fa-picture-o"></i> <span class="menu-item-parent">Gallery</span></a>-->
+ <!--</li>-->
+ </ul>
+ */
+
+(function () {
+    window.menuElements = [
+        {
+            href: 'index',
+            title: 'Home',
+            icon: 'home',
+            text: 'Home'
+        },
+        {
+            href: 'projects',
+            title: 'Projects',
+            icon: 'file-text-o',
+            text: 'Projects'
+        }
+    ];
+})();
---
+++ js/dynamic-menu.js
@@ -0,0 +1,45 @@
+(function () {
+    'use strict';
+
+    var currentLocation = window.location.href.split('/').reverse()[0].replace('.html', '');
+
+    var nav = $('#left-panel').find('> nav');
+
+    var ul = $('<ul></ul>');
+
+    function MenuElement(element) {
+        var self = this;
+
+        self.href = element.href;
+
+        self.active = (element.href == currentLocation);
+
+        self.title = element.title;
+
+        self.icon = element.icon;
+
+        self.text = element.text;
+
+        self.toHTML = function () {
+            var liClass = (self.active) ? ' class="active"' : '';
+            return '<li ' + liClass + '>' +
+                '<a href="' + self.href + '.html" title="' + self.title + '">' +
+                '<i class="fa fa-lg fa-fw fa-' + self.icon + '"></i> ' +
+                '<span class="menu-item-parent">' + self.text + '</span>' +
+                '</a>' +
+                '</li>';
+        }
+    }
+
+    var elements = window.menuElements;
+
+    var w = '';
+    for (var i = 0; i < elements.length; i++) {
+        var element = new MenuElement(elements[i]);
+        w += element.toHTML();
+    }
+
+    ul.html(w);
+
+    nav.append(ul);
+})();
