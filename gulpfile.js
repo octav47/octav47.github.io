94db57311d1b50d9fdac71180737f3aabb4285b6
tribunskiy <forty-7@yandex.ru>
2016-08-29 16:48:37 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,21 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var htmlmin = require('gulp-htmlmin');
 
-gulp.task('minify', function() {
+gulp.task('minify', ['minify-index', 'minify-projects']);
+
+gulp.task('minify-index', function() {
     return gulp.src('index_source.html')
         .pipe(rename('index.html'))
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true
+        }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('minify-projects', function() {
+    return gulp.src('projects_source.html')
+        .pipe(rename('projects.html'))
         .pipe(htmlmin({
             collapseWhitespace: true,
             removeComments: true
