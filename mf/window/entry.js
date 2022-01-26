8a5d652c0cc26dafcfe07ce1d19b4e661e1c6195
ktribunsky <ktribunsky@moysklad.ru>
2022-01-26 20:07:13 +0300

    update

--- mf/window/entry.js
+++ mf/window/entry.js
@@ -1,20 +1,20 @@
-;(function(w) {
-	w.__entry__ = function($) {
-		$.getJSON(
-			'http://tribunsky.com/mf/window/dist/parcel-manifest.json',
-			function(manifest) {
-				var indexjs = manifest['index.js']
-				var style =
-					manifest['../node_modules/bootstrap/dist/css/bootstrap.min.css']
+(function(w) {
+  w.__entry__ = function($) {
+    $.getJSON(
+      "https://tribunsky.com/mf/window/dist/parcel-manifest.json",
+      function(manifest) {
+        var indexjs = manifest["index.js"];
+        var style =
+          manifest["../node_modules/bootstrap/dist/css/bootstrap.min.css"];
 
-				$.getScript('http://tribunsky.com/mf/window/dist' + indexjs)
+        $.getScript("https://tribunsky.com/mf/window/dist" + indexjs);
 
-				$('<link/>', {
-					rel: 'stylesheet',
-					type: 'text/css',
-					href: 'http://tribunsky.com/mf/window/dist' + style,
-				}).appendTo('head')
-			}
-		)
-	}
-})(window)
+        $("<link/>", {
+          rel: "stylesheet",
+          type: "text/css",
+          href: "https://tribunsky.com/mf/window/dist" + style
+        }).appendTo("head");
+      }
+    );
+  };
+})(window);
